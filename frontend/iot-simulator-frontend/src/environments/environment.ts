4c3b59a2dbe918904f9babbbd78f0f817028748c
Aman <yadavaman2282000@gmail.com>
2025-11-10 15:05:57 +0530

    api changes and fixes

--- frontend/iot-simulator-frontend/src/environments/environment.ts
+++ frontend/iot-simulator-frontend/src/environments/environment.ts
@@ -1,9 +1,12 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:3000/api', // Backend URL (for simulation endpoints only)
+  backendUrl: 'http://34.93.247.3:3000/api', // Backend URL (for simulation endpoints)
   profilesUrl: 'https://wiyfcvypeifbdaqnfgrr.supabase.co/rest/v1/profiles',
   elderlyPersonsUrl: 'https://wiyfcvypeifbdaqnfgrr.supabase.co/rest/v1/elderly_persons',
   devicesUrl: 'https://wiyfcvypeifbdaqnfgrr.supabase.co/rest/v1/devices',
+  deviceDataUrl: 'https://wiyfcvypeifbdaqnfgrr.supabase.co/rest/v1/device_data',
+  deviceTypeDataConfigsUrl: 'https://wiyfcvypeifbdaqnfgrr.supabase.co/rest/v1/device_type_data_configs',
+  deviceTypesUrl: 'https://wiyfcvypeifbdaqnfgrr.supabase.co/rest/v1/device_types',
   profilesApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpeWZjdnlwZWlmYmRhcW5mZ3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4MDEyNDYsImV4cCI6MjA3NTM3NzI0Nn0._xA81aVu_on-OL-axvskm4aCCywkIZliIiXGFu4ey2c',
   ingestUrl: 'https://wiyfcvypeifbdaqnfgrr.supabase.co/functions/v1/device-ingest'
 };
